ad70613726fa1d4e6b8f1bf493234486ae9dfb60
Nathan Hogg <nathanhogg1223@gmail.com>
2026-08-31 16:07:15 -0400

    setup(MF): add Mujoco

--- scripts/test_mujoco.py
+++ scripts/test_mujoco.py
@@ -1,1 +1,6 @@
-# mujoco env test
+import json
+import mujoco
+import platform
+
+print(json.dumps({'python': platform.python_version(), 'mujoco':
+                  mujoco.__version__}, sort_keys=True))
